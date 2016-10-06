2d8e61d07586a56eb29568a4535ceed1dceb5526
karlovk <konstantinkarlov@gmail.com>
2016-10-06 06:26:14 +0300

    updated files

--- APLesson_04/Ex_03.java
+++ APLesson_04/Ex_03.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 public class Ex_03
 {
-	
-	public static void main(String[]args)
-	{
-		RetMethods average = new RetMethods();
-		int rate = 12;
-		int principal = 34;
-		int time = 56;
-		int number = 78;
-		
-		
-		System.out.println("Your monthly will be" + payment(rate, principal, number, time));
-		
-	}
-	
-	public double payment(double r, double p, double n, double t)
-	{
-		return ( p * ( Math.pow(1+r/n, n*t)))/(t*12);
-		
-	}
-	
-	
+    
+    public static void main(String[]args)
+    {
+        
+        Ex_03 payment = new Ex_03();
+        int rate = 12;
+        int principal = 34;
+        int time = 56;
+        int number = 78;
+        
+        
+        System.out.println("Your monthly will be " + payment (rate, principal, time, number));
+        
+    }
+    
+    public double payment(double rate, double principal, double number, double time)
+    {
+        return ( p * ( Math.pow (1+r/n, n*t)))/(t*12);
+        
+    }
+    
+    
 }
