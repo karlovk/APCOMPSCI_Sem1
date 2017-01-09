4ddad31b1c710e84795cba25793dc979d61d4ba0
karlovk <konstantinkarlov@gmail.com>
2017-01-09 09:09:16 -0800

    Updated Files

---
+++ APLesson_11/Lab_11_Rand_Nums.java
@@ -0,0 +1,26 @@
+public class Lab_11_Rand_Nums
+{
+	public static void main (String[]args)
+	{
+		int [][] values = new int[4][4];
+		values [0][0] = 1;
+		values [1][1] = 2;
+		values [2][2] = 3;
+		values [3][3] = 4;
+		
+		for(int i = 0; i < values.length; i++);
+		{
+			for(int j = 0; j < values [j].lenght;j++)
+			{
+				
+				
+				System.out.println(values[i][j]);
+				
+			}	
+			
+		}
+		
+		
+	}
+	
+}
